5f707f285060ed65e63aac5dc1201a6a4e3ebe65
AnshSheladiya <anshsheladiya83@gmail.com>
2023-03-05 19:14:40 +0530

    Add CSS to ToDo React project

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,65 +1,53 @@
-// TodoList.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import './TodoList.css';
 
 function TodoList() {
-const [inputValue, setInputValue] = useState("");
-const [todoList, setTodoList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
-useEffect(() => {
-    const storedTodoList = JSON.parse(localStorage.getItem("todoList"));
-    if (storedTodoList) {
-      setTodoList(storedTodoList);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodoList([...todoList, { task: inputValue, completed: false }]);
+      setInputValue("");
     }
-  }, []);
-    
-  useEffect(() => {
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-  }, [todoList]);
-  
+  };
 
-const handleInputChange = (event) => {
-setInputValue(event.target.value);
-};
+  const handleCompleteTodo = (index) => {
+    const newTodoList = [...todoList];
+    newTodoList[index].completed = true;
+    setTodoList(newTodoList);
+  };
 
-const handleAddTodo = () => {
-if (inputValue.trim() !== "") {
-setTodoList([...todoList, { task: inputValue, completed: false }]);
-setInputValue("");
-}
-};
+  const handleRemoveCompleted = () => {
+    setTodoList(todoList.filter((todo) => !todo.completed));
+  };
 
-const handleCompleteTodo = (index) => {
-const newTodoList = [...todoList];
-newTodoList[index].completed = true;
-setTodoList(newTodoList);
-};
-
-const handleRemoveCompleted = () => {
-setTodoList(todoList.filter((todo) => !todo.completed));
-};
-
-return (
-<div>
-<h1>TODO List</h1>
-<input type="text" value={inputValue} onChange={handleInputChange} />
-<button onClick={handleAddTodo}>Add Todo</button>
-<ul>
-{todoList.map((todo, index) => (
-<li key={index}>
-<input
-type="checkbox"
-checked={todo.completed}
-onChange={() => handleCompleteTodo(index)}
-/>
-<span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-{todo.task}
-</span>
-</li>
-))}
-</ul>
-<button onClick={handleRemoveCompleted}>Remove Completed</button>
-</div>
-);
+  return (
+    <div className='container'>
+      <h1 className='title-header'>TODO List</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todoList.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleCompleteTodo(index)}
+            />
+            <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+              {todo.task}
+            </span>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleRemoveCompleted}>Remove Completed</button>
+    </div>
+  );
 }
 
 export default TodoList;
